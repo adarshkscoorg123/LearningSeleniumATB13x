8b701801b5ad87f4d814bd9d5cbde48468fc03a8
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-10-08 18:24:50 +0530

    Data Driven testing

---
+++ src/test/java/com/thetestingacademy/ex_17_Data_driven_testing_POI/Lab_53_DDT_TestNG_POI.java
@@ -0,0 +1,22 @@
+package com.thetestingacademy.ex_17_Data_driven_testing_POI;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Lab_53_DDT_TestNG_POI {
+
+    @Test (dataProvider = "getData")
+    public void test_vwo_login(String username, String password, String ER){
+        System.out.println("Running");
+        System.out.println(username + "-" + password + "-" + ER);
+    }
+
+    @DataProvider
+    public Object[][] getData(){
+        // Read  from the Excel file
+        //convert this into the 2d array Object
+        // return that array
+        return null;
+        };
+    }
+
---
+++ src/test/java/com/thetestingacademy/ex_17_Data_driven_testing_POI/Lab_54_DDT_TestNG_POI.java
@@ -0,0 +1,22 @@
+package com.thetestingacademy.ex_17_Data_driven_testing_POI;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Lab_54_DDT_TestNG_POI {
+
+    @Test (dataProvider = "getData")
+    public void test_vwo_login(String username, String password){
+        System.out.println("Running");
+        System.out.println(username + "-" + password);
+    }
+
+    @DataProvider
+    public Object[][] getData(){
+        // Read  from the Excel file
+        //convert this into the 2d array Object
+        // return that array
+        return UtilExcel.getTestDataFromExcel("sheet1");
+        };
+    }
+
